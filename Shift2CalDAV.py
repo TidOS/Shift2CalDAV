cc3bd3688402747c9301f159a3a1a9a0b56e3901
jordan <jordan@jmthomas.tech>
2020-02-24 21:54:13 -0600

    added ics, can create an event, will add code to push to a calendar next

--- Shift2CalDAV.py
+++ Shift2CalDAV.py
@@ -3,6 +3,7 @@
 #pip install selenium
 #pip install httplib2
 #pip install caldav
+#pip install ics
 
 from selenium import webdriver
 import time
@@ -12,37 +13,38 @@ from httplib2 import Http
 import caldav
 from caldav.elements import dav, cdav
 import configparser
+from ics import Calendar, Event
 
 #temporary holder for config options, move to file later
 config = configparser.ConfigParser()
 config.read("credentials.cfg")
-davurl = config['url']['address']
+davurl = config['url']['address-test']
 
-
-vcal = """BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//Example Corp.//CalDAV Client//EN
-BEGIN:VEVENT
-UID:1234567890
-DTSTAMP:20100510T182145Z
-DTSTART:20100512T170000Z
-DTEND:20100512T180000Z
-SUMMARY:This is an event
-END:VEVENT
-END:VCALENDAR
-"""
+#example vcal
+#vcal = """BEGIN:VCALENDAR
+#VERSION:2.0
+#PRODID:-//Example Corp.//CalDAV Client//EN
+#BEGIN:VEVENT
+#UID:1234567890
+#DTSTAMP:20100510T182145Z
+#DTSTART:20100512T170000Z
+#DTEND:20100512T180000Z
+#SUMMARY:This is an event
+#END:VEVENT
+#END:VCALENDAR
+#"""
 client = caldav.DAVClient(davurl)
 principal = client.principal()
 calendars = principal.calendars()
-if len(calendars) > 0:
-    calendar = calendars[0]
-    print( "Using calendar", calendar)
 
-    print ("Looking for events in 2020-02")
-    results = calendar.date_search(
-        datetime(2020, 2, 1), datetime(2020, 3, 1))
+ics = Calendar()
+event = Event()
+event.name = "a test event"
+event.begin = '2020-02-28 12:30:00'
+ics.events.add(event)
+ics.events
+print(ics)
 
-    for event in results:
-        print("Found", event)
-else:
-     print("nada")
+#we are currently providing the exact calendar to use in our URL
+calendar = calendars[0]
+#calendar.add_event(someical)
