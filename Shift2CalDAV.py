df7b8cc9a9b789453a92393ff275b8ac289101e2
jordan <jordan@jmthomas.tech>
2020-02-24 23:28:37 -0600

    Logging in cannot be automated easily, so for now we are manually logging in - you get 30 seconds before the script continues.
    It is now able to pull days and times in the format:
    Day
    Date
    Workcenter
    Start - End Time
    Store number
    
    The next step is to convert each of these to a vcal event and then push it out, almost at "kind of functional" level now

--- Shift2CalDAV.py
+++ Shift2CalDAV.py
@@ -34,10 +34,15 @@ print(davurl)
 #END:VEVENT
 #END:VCALENDAR
 #"""
+
+#acquire calendar info
 client = caldav.DAVClient(davurl)
 principal = client.principal()
 calendars = principal.calendars()
+#we are currently providing the exact calendar to use in our URL
+calendar = calendars[0]
 
+#ics is our ical event - in vcal format
 ics = Calendar()
 event = Event()
 event.name = "a test event"
@@ -45,8 +50,52 @@ event.name = "a test event"
 #https://icalendar.org/iCalendar-RFC-5545/3-2-19-time-zone-identifier.html
 event.begin = '2020-02-28 12:30:00'
 ics.events.add(event)
-#print(ics)
-
-#we are currently providing the exact calendar to use in our URL
-calendar = calendars[0]
 calendar.add_event(str(ics))
+
+
+#############################
+#####    CHROME STUFF   #####
+#############################
+
+#chromedriver must be in PATH,
+#todo - detect Chrome version and download correct chromedriver?
+#https://sites.google.com/a/chromium.org/chromedriver/downloads
+chromeDriver = 'chromedriver.exe'
+browser = webdriver.Chrome(chromeDriver)
+browser.get('http://wss.target.com/selfservice')
+
+#sign in manually, be faster than 30 seconds.
+#there is some sort of bizarre trickery to detect auto logins it seems
+#the login page is dynamically generated now, the fields are not predictable
+#once logged in though, eHR is still working okay with chromedriver
+
+
+time.sleep(60)
+
+table = browser.find_element_by_class_name("request_table_bordered")
+
+for x in range(2, 9):
+    # this loop cycles through the week and assembles your shift information and creates the event
+    days = browser.find_element_by_xpath("//*[@id='page_content']/table[1]/tbody/tr[1]/td/table[3]/tbody/tr[1]/td["
+                                         + str(x) + "]")
+    shift = browser.find_element_by_xpath("//*[@id='page_content']/table[1]/tbody/tr[1]/td/table[3]/tbody/tr[3]/td["
+                                          + str(x) + "]")
+    if not shift.text.strip():
+        continue
+    shift_info = (days.text + '\n' + shift.text)
+    print(shift_info)
+
+#move to the next page
+next = browser.find_element_by_xpath("//*[@id='page_content']/table[1]/tbody/tr[1]/td/table[2]/tbody/tr[1]/td/div/a[2]")
+next.click()
+
+for x in range(2, 9):
+    # this loop cycles through the week and assembles your shift information and creates the event
+    days = browser.find_element_by_xpath("//*[@id='page_content']/table[1]/tbody/tr[1]/td/table[3]/tbody/tr[1]/td["
+                                         + str(x) + "]")
+    shift = browser.find_element_by_xpath("//*[@id='page_content']/table[1]/tbody/tr[1]/td/table[3]/tbody/tr[3]/td["
+                                          + str(x) + "]")
+    if not shift.text.strip():
+        continue
+    shift_info = (days.text + '\n' + shift.text)
+    print(shift_info)
